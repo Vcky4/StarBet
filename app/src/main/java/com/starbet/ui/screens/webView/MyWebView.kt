fa580acd43a29d9170c2b15d50f80c8ab6f332f0
vcky4 <okon.victor.u@gmail.com>
2023-11-26 21:22:52 +0100

    fix webview

--- app/src/main/java/com/starbet/ui/screens/webView/MyWebView.kt
+++ app/src/main/java/com/starbet/ui/screens/webView/MyWebView.kt
@@ -1,10 +1,17 @@
 package com.starbet.ui.screens.webView
 
+import android.graphics.Bitmap
+import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.google.accompanist.web.WebView
-import com.google.accompanist.web.rememberWebViewState
+import androidx.compose.ui.viewinterop.AndroidView
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Composable
@@ -13,13 +20,31 @@ fun MyWebView(
     modifier: Modifier = Modifier,
     url: String,
 ) {
-    val webViewState = rememberWebViewState(url = url)
-    WebView(
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView: WebView? = null
+    AndroidView(
         modifier = modifier,
-        state = webViewState,
-        captureBackPresses = true,
-        onCreated = { it : WebView ->
-            it.settings.javaScriptEnabled = true
-        }
-    )
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        backEnabled = view.canGoBack()
+                    }
+                }
+                settings.javaScriptEnabled = true
+
+                loadUrl(url)
+                webView = this
+            }
+        }, update = {
+            webView = it
+        })
+
+    BackHandler(enabled = backEnabled) {
+        webView?.goBack()
+    }
 }
